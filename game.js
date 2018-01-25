24825c723c55785bf826dccd35395f4793983718
Daniel Voyce <dan@voyced.com>
2018-01-25 22:18:30 +0000

    Final test - adding cells WIP

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 
 const store = {
     dims: {
-        width: 50,
-        height: 50,
+        width: 20,
+        height: 20,
     },
     filledCells: [],
-}
+};
 
 function populateCells(cellsArray = []) {
     store.filledCells = cellsArray;
@@ -15,9 +15,53 @@ function reset() {
     store.filledCells = [];
 }
 
-function next() {
+const array = new Array(20).fill();
+
+const allCells = [];
+
+array.forEach((a, x) => {
+    array.forEach((b, y) => {
+        allCells.push({
+            x: x + 1,
+            y: y + 1,
+        });
+    });
+});
+
+
+
+function addCells() {
+    const filledCellStrings = store.filledCells.map((c) => JSON.stringify(c));
+    allCells.forEach((cell) => {
+        const filledCellIndex = filledCellStrings.indexOf(JSON.stringify(cell));
+
+        // landed on a filled cell
+        if (filledCellIndex > -1) {
+            store.filledCells.forEach((mainCell) => {
+                const filledNeighbours = store.filledCells.filter((otherCell) => {
+                    const xDiff = Math.abs(mainCell.x - otherCell.x);
+                    const yDiff = Math.abs(mainCell.y - otherCell.y);
+
+                    const isSelf = (xDiff + yDiff === 0);
+
+                    const isNeighbour = (xDiff <= 1 && yDiff <= 1);
+                    return !isSelf && isNeighbour;
+                });
+
+                console.log('filledNeighbours:', filledNeighbours);
+
+
+                if (filledNeighbours.length >= 3) {
+                    store.filledCells.push(cell);
+                }
+            });
+        }
+    });
+}
+
+function removeCells() {
     store.filledCells = store.filledCells.filter((mainCell) => {
-        const neighbours = store.filledCells.filter((otherCell) => {
+        const filledNeighbours = store.filledCells.filter((otherCell) => {
             const xDiff = Math.abs(mainCell.x - otherCell.x);
             const yDiff = Math.abs(mainCell.y - otherCell.y);
 
@@ -31,12 +75,11 @@ function next() {
         // console.log('neighbours:', neighbours);
         // console.log('=============================');
 
-        if (neighbours.length <= 1) {
+        if (filledNeighbours.length <= 1) {
             return false;
         }
 
-
-        if (neighbours.length >= 4) {
+        if (filledNeighbours.length >= 4) {
             return false;
         }
 
@@ -47,6 +90,7 @@ function next() {
 module.exports = {
     store,
     populateCells,
-    next,
+    removeCells,
+    addCells,
     reset,
 };
--- game.test.js
+++ game.test.js
@@ -28,7 +28,7 @@ describe('For a space that is "populated":', () => {
 
         expect(game.store.filledCells.length).toBe(4);
 
-        game.next();
+        game.removeCells();
         //  X00X
         expect(game.store.filledCells.length).toEqual(2);
         expect(game.store.filledCells[0]).toEqual({ x: 10, y: 11 });
@@ -69,7 +69,7 @@ describe('For a space that is "populated":', () => {
             },
         ]);
 
-        game.next();
+        game.removeCells();
 
         // 00
         // XX
@@ -124,7 +124,7 @@ describe('For a space that is "populated":', () => {
             },
         ]);
 
-        game.next();
+        game.removeCells();
 
         // XXX
         // 0X0
@@ -159,8 +159,65 @@ describe('For a space that is "populated":', () => {
             },
         ]);
 
-        game.next();
+        game.removeCells();
 
         expect(game.store.filledCells.length).toEqual(4);
+        expect(game.store.filledCells[0]).toEqual({ x: 11, y: 10 });
+        expect(game.store.filledCells[1]).toEqual({ x: 10, y: 11 });
+        expect(game.store.filledCells[2]).toEqual({ x: 12, y: 11 });
+        expect(game.store.filledCells[3]).toEqual({ x: 11, y: 12 });
+    });
+});
+
+describe('For a space that is "empty" or "unpopulated"', () => {
+    beforeEach(() => {
+        game.reset();
+    });
+
+    test('Each cell with three neighbors becomes populated.', () => {
+
+        // 00
+        // 00
+        // 00
+        game.populateCells([
+            {
+                x: 10,
+                y: 10,
+            },
+            {
+                x: 11,
+                y: 10,
+            },
+            {
+                x: 10,
+                y: 11,
+            },
+            {
+                x: 11,
+                y: 11,
+            },
+            {
+                x: 10,
+                y: 12,
+            },
+            {
+                x: 11,
+                y: 12,
+            },
+        ]);
+
+        game.removeCells();
+        game.addCells();
+
+        // X00X
+        // 0XX0
+        // X00X
+        expect(game.store.filledCells.length).toEqual(6);
+        // expect(game.store.filledCells[0]).toEqual({ x: 10, y: 10 });
+        // expect(game.store.filledCells[1]).toEqual({ x: 11, y: 10 });
+        // expect(game.store.filledCells[2]).toEqual({ x: 10, y: 12 });
+        // expect(game.store.filledCells[3]).toEqual({ x: 11, y: 12 });
+        // expect(game.store.filledCells[4]).toEqual({ x: 11, y: 10 });
+        // expect(game.store.filledCells[5]).toEqual({ x: 11, y: 12 });
     });
 });
